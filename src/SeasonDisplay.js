a353935ec74cc2708ba757c1c3a7621bb60c070a
Matt Chisholm <mathesonchisholm@hotmail.com>
2022-04-17 11:35:24 -0700

    conditionally rendering seasondisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export default function SeasonDisplay() {
-  return (
-    <div>Season Display</div>
-  )
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter";
+  } else {
+    return lat > 0 ? "Winter" : "Summer";
+  }
 };
+
+export default function SeasonDisplay(props) {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  return (
+    <div>
+      {season === "winter" ? "Burr, its chilly!" : "Lets hit the beach!"}
+    </div>
+  );
+}
